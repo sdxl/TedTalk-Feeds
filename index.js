bb1680ad718bd18d30579163246d439d3b0785c7
Eric <hoang.e.nguyen@gmail.com>
2015-12-01 15:42:19 -0800

    Add preloader to index

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 google.load("feeds", "1");
-
 function initialize() {
   var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
   feed.setNumEntries(100)
   feed.load(function(result) {
-    console.log('entries are', result.feed.entries[0].mediaGroups[0].contents[0].thumbnails[0])
-      var container = $('#feed');
-      for (var i = 0; i < result.feed.entries.length; i++) {
-        var entry = result.feed.entries[i];
-        var img = entry.mediaGroups[0].contents[0].thumbnails[0].url
-        var item = $('<li>'+ 
-                '<div class="collapsible-header" onclick="removeSnippet(this)">'+
-                  '<div >' + 
-                    '<h5>' + entry.title + '</h5>'+
-                    '<h6>' + 
-                    '<div>'+ 'Posted: ' + new Date(entry.publishedDate) + '</div>'+
-                    '</h6>'+
-                    '<span/>' + entry.contentSnippet  + '</span>' +
-                  '</div>'+
+    var container = $('#feed');
+    for (var i = 0; i < result.feed.entries.length; i++) {
+      var entry = result.feed.entries[i];
+      var img = entry.mediaGroups[0].contents[0].thumbnails[0].url
+      var item = $('<li>'+ 
+              '<div class="collapsible-header">'+
+                '<div >' + 
+                  '<h5>' + entry.title + '</h5>'+
+                  '<h6>' + 
+                  '<div>'+ 'Posted: ' + new Date(entry.publishedDate) + '</div>'+
+                  '</h6>'+
                 '</div>'+
-                '<div class="collapsible-body">'+
-                  '<p>'+entry.content + '</p>'+
-                  '<p>'+ '<a href=' +entry.link  + ' target="_blank">' + "click here to view in new tab" + '</a>'+ '</p>'+
-                '<div>'
-            +'</li>'
-          );
-        item.appendTo(container);
-      }
+              '</div>'+
+              '<div class="collapsible-body">'+
+                '<p>'+entry.content + '</p>'+
+                '<p>'+ '<a href=' +entry.link  + ' target="_blank">' + "click here to view video in new tab" + '</a>'+ '</p>'+
+              '<div>'
+          +'</li>'
+        );
+      item.appendTo(container);
+    }
   });
+  $("#loader").remove();
 }
-
 google.setOnLoadCallback(initialize);
+
