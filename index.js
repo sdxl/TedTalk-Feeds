5f370905a1011a175d35fae44e07897696d8ca32
Eric <hoang.e.nguyen@gmail.com>
2015-12-01 16:54:38 -0800

    Add error message

--- index.js
+++ index.js
@@ -4,29 +4,35 @@ function initialize() {
   feed.setNumEntries(100)
   feed.load(function(result) {
     var container = $('#feed');
-    for (var i = 0; i < result.feed.entries.length; i++) {
-      var entry = result.feed.entries[i];
-      var img = entry.mediaGroups[0].contents[0].thumbnails[0].url
-      var item = $('<li>'+ 
-              '<div class="collapsible-header">'+
-                '<div >' + 
-                  '<h5>' + entry.title + '</h5>'+
-                  '<h6>' + 
-                  '<div>'+ 'Posted: ' + new Date(entry.publishedDate) + '</div>'+
-                  '</h6>'+
-                  '<img src=' + img + '>' +
+    if (!result.error) {
+      for (var i = 0; i < result.feed.entries.length; i++) {
+        var entry = result.feed.entries[i];
+        var img = entry.mediaGroups[0].contents[0].thumbnails[0].url
+        var item = $('<li>'+ 
+                '<div class="collapsible-header">'+
+                  '<div >' + 
+                    '<h5>' + entry.title + '</h5>'+
+                    '<h6>' + 
+                    '<div>'+ 'Posted: ' + new Date(entry.publishedDate) + '</div>'+
+                    '</h6>'+
+                    '<img src=' + img + '>' +
+                  '</div>'+
                 '</div>'+
-              '</div>'+
-              '<div class="collapsible-body">'+
-                '<p>'+entry.content + '</p>'+
-                '<p>'+ '<a href=' +entry.link  + ' target="_blank">' + "click here to view video in new tab" + '</a>'+ '</p>'+
-              '<div>'
-          +'</li>'
-        );
-      item.appendTo(container);
+                '<div class="collapsible-body">'+
+                  '<p>'+entry.content + '</p>'+
+                  '<p>'+ '<a href=' +entry.link  + ' target="_blank">' + "click here to view video in new tab" + '</a>'+ '</p>'+
+                '<div>'
+            +'</li>'
+          );
+        item.appendTo(container);
+      }
+      $("#loader").remove();
+    }else{
+      $("#loader").remove();
+      var errorMessage = $('<div>' + result.error.message + '</div>')
+      container.prepend(errorMessage)
     }
   });
-  $("#loader").remove();
 }
 google.setOnLoadCallback(initialize);
 
